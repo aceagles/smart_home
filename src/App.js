c56e211d8775a5dc7df6466dcb2926713c29cb3e
Adam Eagles <ad.c.eagles@gmail.com>
2022-05-23 22:07:16 +0100

    Change to better implementation of the map function

--- src/App.js
+++ src/App.js
@@ -4,30 +4,19 @@ import React, {useState, useEffect} from 'react'
 function App() {
    const [SwitchList, setSwitchList] = useState([])
 
+  //Fetch the list of switches
    useEffect(() => {
-     let switchJSON = []
      fetch("http://localhost:3001/switches/")
       .then(dat => dat.json())
-      .then(dat => {
-        switchJSON = dat;
-        console.log(dat);
-        const entries = switchJSON.map(entry => {
-          console.log(entry)
-          return <SwitchEntry key={entry.name} switchInfo={entry} />
-        }
-        );
-    setSwitchList(() => entries);
-    return true
-      })
-    
-    
-     
+      .then(dat => setSwitchList(dat))  
    }, [])
    
 
   return (
     <div className="App">
-      {SwitchList}
+      {SwitchList && SwitchList.map(entry => 
+        <SwitchEntry key={entry.name} switchInfo={entry} />
+      )}
     </div>
   );
 }
