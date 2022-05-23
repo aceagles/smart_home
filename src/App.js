615fb91a01cecf796fc76fa9753a4017edf788a0
Adam Eagles <ad.c.eagles@gmail.com>
2022-05-23 22:03:37 +0100

    sample data into dB and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import SwitchEntry from './SwitchEntry';
-
+import React, {useState, useEffect} from 'react'
 
 function App() {
+   const [SwitchList, setSwitchList] = useState([])
+
+   useEffect(() => {
+     let switchJSON = []
+     fetch("http://localhost:3001/switches/")
+      .then(dat => dat.json())
+      .then(dat => {
+        switchJSON = dat;
+        console.log(dat);
+        const entries = switchJSON.map(entry => {
+          console.log(entry)
+          return <SwitchEntry key={entry.name} switchInfo={entry} />
+        }
+        );
+    setSwitchList(() => entries);
+    return true
+      })
+    
+    
+     
+   }, [])
+   
+
   return (
     <div className="App">
-    
-      <SwitchEntry />
+      {SwitchList}
     </div>
   );
 }
--- src/SwitchEntry.js
+++ src/SwitchEntry.js
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from 'react'
-import './SwitchEntry.js'
+import './SwitchEntry.css'
 
-export default function SwitchEntry(switchInfo) {
+export default function SwitchEntry({key, switchInfo}) {
    const [isOn, setIsOn] = useState(false)
-   
    const updateStatus = () => {
     fetch(switchInfo.statusUrl)
-    .then(dat => {setIsOn(dat); console.log(dat)})
+    .then(dat => dat.json())
+    .then(dat => {setIsOn(dat.status)})
    }
    
    // Mount and unmount functions
    useEffect(() => {
-     let interval = setInterval(updateStatus, 1000)
+     updateStatus();
+     let interval = setInterval(updateStatus, 3000)
      return () => {
          clearInterval(interval)
      }
@@ -19,8 +20,8 @@ export default function SwitchEntry(switchInfo) {
 
    
   return (
-    <div>
-        <h1>{switchInfo.name} - <div class="link">{isOn ? "On" : "Off"}</div></h1>
+    <div className="switchBox">
+      {switchInfo.name} - <div className="link">{isOn ? "On" : "Off"}</div>
     </div>
   )
 }
