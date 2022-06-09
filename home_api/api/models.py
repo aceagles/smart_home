bdc44afa39e4afd8625a4c480387a152354de105
Adam Eagles <ad.c.eagles@gmail.com>
2022-06-09 20:49:01 +0100

    create smartswitch model

--- home_api/api/models.py
+++ home_api/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class SmartSwitch(models.Model):
+    status_url = models.URLField()
+    toggle_url = models.URLField()
+    slug = models.SlugField()
+    name = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.brand_name)
+        super(SmartSwitch, self).save(*args, **kwargs)
