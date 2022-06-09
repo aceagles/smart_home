0a223404e07b3fe38b347db024ae3da5866f2d23
Adam Eagles <ad.c.eagles@gmail.com>
2022-06-09 21:43:03 +0100

    Admin formatting

--- home_api/api/models.py
+++ home_api/api/models.py
@@ -11,3 +11,9 @@ class SmartSwitch(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(SmartSwitch, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
