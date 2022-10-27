124fe9ef96e99e59e01d254e32d6e932e6368297
Adam Eagles <ad.c.eagles@gmail.com>
2022-10-27 22:08:46 +0100

    remove url to unauthenticated api endpoint

--- home_api/heating_control/urls.py
+++ home_api/heating_control/urls.py
@@ -5,6 +5,5 @@ from .api import ControllerView
 urlpatterns = [
     path("", views.toggle_form),
     path("btn", views.toggle_btn, name="toggle_button"),
-    path("usage", views.update_usage),
     path("check", ControllerView.as_view())
 ]
